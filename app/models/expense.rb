7e7320b9c892a468bf09066295ee2525ea4102df
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-17 08:25:44 +0300

    Fix linter errors

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,5 +5,5 @@ class Expense < ApplicationRecord
   has_many :categories, through: :category_expenses, dependent: :destroy
 
   validates :name, presence: true, length: { in: 3..50 }
-  validates :amount, presence: true, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
